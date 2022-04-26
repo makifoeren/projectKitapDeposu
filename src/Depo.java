7a17cf80748240c18a03ec2851ca888012c1c366
makifoeren <makif.oeren@gmail.com>
2022-04-26 10:01:04 +0200

    26.04

--- src/Depo.java
+++ src/Depo.java
@@ -17,4 +17,46 @@ public class Depo {
         this.yazarAdi = yazarAdi;
         this.kitapFiyat = kitapFiyat;
     }
+
+    public int getKitapNo() {
+        return kitapNo;
+    }
+
+    public void setKitapNo(int kitapNo) {
+        this.kitapNo = kitapNo;
+    }
+
+    public String getKitapAdi() {
+        return kitapAdi;
+    }
+
+    public void setKitapAdi(String kitapAdi) {
+        this.kitapAdi = kitapAdi;
+    }
+
+    public String getYazarAdi() {
+        return yazarAdi;
+    }
+
+    public void setYazarAdi(String yazarAdi) {
+        this.yazarAdi = yazarAdi;
+    }
+
+    public int getKitapFiyat() {
+        return kitapFiyat;
+    }
+
+    public void setKitapFiyat(int kitapFiyat) {
+        this.kitapFiyat = kitapFiyat;
+    }
+
+    @Override
+    public String toString() {
+        return "Depo{" +
+                "kitapNo=" + kitapNo +
+                ", kitapAdi='" + kitapAdi + '\'' +
+                ", yazarAdi='" + yazarAdi + '\'' +
+                ", kitapFiyat=" + kitapFiyat +
+                '}';
+    }
 }
